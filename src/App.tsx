601bd8e33d17a567f9506be01c96309bda6ad41b
Geeky Coder <toyibe25@gmail.com>
2022-10-20 23:32:10 +0100

    update fontawesome icons path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
-import './styles/fontawesome-free-6.1.1-web/css/all.css';
 import Layout from './components/Layout';
 import ScrollToTop from './components/ScrollToTop';
 
